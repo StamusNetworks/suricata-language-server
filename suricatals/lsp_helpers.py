d72f4f3fd12b87bd27a7a9b73a50cefca0f414cf
Eric Leblond <el@stamus-networks.com>
2022-12-28 14:12:44 +0100

    set copyright on python files

--- suricatals/lsp_helpers.py
+++ suricatals/lsp_helpers.py
@@ -1,3 +1,24 @@
+"""
+Copyright(C) 2022-2023 Stamus Networks
+Written by Eric Leblond <el@stamus-networks.com>
+
+This file is part of Suricata Language Server.
+
+Suricata Language Server is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Suricata Language Server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Suricata Language Server.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 class FileRange:
     def __init__(self, line_start, col_start, line_end, col_end):
         self.line_start = line_start
