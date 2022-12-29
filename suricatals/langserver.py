97bf198663b93468d429ce4f02465af6646d47fa
Eric Leblond <el@stamus-networks.com>
2022-12-29 18:30:08 +0100

    langserver: add batch command

--- suricatals/langserver.py
+++ suricatals/langserver.py
@@ -22,6 +22,7 @@ import logging
 import os
 import traceback
 import re
+import json
 
 from suricatals.jsonrpc import path_from_uri
 from suricatals.parse_signatures import SuricataFile
@@ -412,6 +413,11 @@ class LangServer:
             code=-32601,
             message="method {} not found".format(request["method"]))
 
+    def analyse_file(self, filepath):
+        file_obj = SuricataFile(filepath, self.rules_tester)
+        file_obj.load_from_disk()
+        for diag in file_obj.check_file():
+            print(json.dumps(diag.to_message()))
 
 class JSONRPC2Error(Exception):
     def __init__(self, code, message, data=None):
