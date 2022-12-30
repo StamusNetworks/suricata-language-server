aac5168851f936829349677362f59b6df2511f77
Nicolas Frisoni <nfrisoni@stamus-networks.com>
2022-12-30 10:25:03 +0100

    core: remove python 2 compatibility code

--- suricatals/__init__.py
+++ suricatals/__init__.py
@@ -20,7 +20,6 @@ You should have received a copy of the GNU General Public License
 along with Suricata Language Server.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from __future__ import print_function
 import sys
 import argparse
 from .langserver import LangServer
