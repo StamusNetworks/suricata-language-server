22913f62b60da1019b7c72bb1947fe5322686f14
Eric Leblond <el@stamus-networks.com>
2024-10-10 14:23:01 +0200

    setup: set release to 0.9.5

--- suricatals/__init__.py
+++ suricatals/__init__.py
@@ -25,7 +25,7 @@ import argparse
 from suricatals.langserver import LangServer
 from suricatals.jsonrpc import JSONRPC2Connection, ReadWriter
 import json
-__version__ = '0.9.4'
+__version__ = '0.9.5'
 
 
 def error_exit(error_str):
